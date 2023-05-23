873503064f36762612d629c914f7ca704b08da40
Shiv Patel <84260195+shivpatel22@users.noreply.github.com>
2023-05-23 22:28:22 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,33 @@
+<?php include_once "./include/header.php"; ?>
+
+<div class="container">
+    <div class="row">
+        <img src="images/" class="col-4 card-img-top" alt="Logo">
+
+        <div class="container col-8" style="margin-top: 50px;">
+            <div class="card">
+                <div class="card-header text-center">
+                    <strong>Home Service Helpdesk</strong>
+                </div>
+
+                <div class="card-body">
+
+                <strong class="card-title">Project Members</strong>
+                <ul>
+                    <li>Shiv Patel</li>
+                    <li>Parthiv sutariya</li>
+                    <li>Jevik chopda</li>
+                    <li>Dhruvit mistry</li>
+                </ul>
+            </div>
+
+            <div class="card-footer text-center">
+                Project-Guide: Prof. Virendra Barot
+            </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<?php include_once "./include/footer.php"; ?>
